216f50be1210230ab0087b754be30cbb2ad98736
mahutt <tommy.mahut@gmail.com>
2024-10-17 11:15:38 -0400

    Make corners more rounded on cards

--- app/components/GlowCard.js
+++ app/components/GlowCard.js
@@ -1,6 +1,6 @@
 export default function GlowCard({ children }) {
     return (
-        <div className="border border-gray-800 rounded-lg p-4 hover:shadow-glow transition-shadow duration-200 linear">
+        <div className="border border-gray-800 rounded-xl p-5 hover:shadow-glow transition-shadow duration-200 linear">
             {children}
         </div>
     );
